3ae7a0139517e80f3887de642ea686f479103ce8
CJ Rhone <charlesrhonejr@gmail.com>
2018-03-30 11:12:19 -0700

    Add basic functionality of returning 1 of 4 values depending on user input

--- js/scripts.js
+++ js/scripts.js
@@ -19,10 +19,31 @@ $(document).ready(function() {
 
 var robotize = function(number) {
 
-if ( number === 1 ) {
-  return "boop";
-} else {
-  return "beep";
-}
+  console.log("booting up");
+//
+// var conditions = [1, 0, 3];
+//
+//   for (var i=0; i < number.length; i++) {
 
-}
+    console.log("Roboting");
+
+    if (number === 1){ //'1'
+
+      return "1";
+
+    }
+    if ( number === 0) { // '0'
+
+      return "0";
+
+    }
+
+    if ( number === 3 || number % 3 == 0 ) {
+
+      return "I'm sorry Dave, I can't do that.";
+
+    } else {
+
+      return "BZZT. Does not compute.";
+    };
+  };
