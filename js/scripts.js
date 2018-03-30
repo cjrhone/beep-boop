0a1ca8cb57129caec188febd2fec7c349643c8dc
CJ Rhone <charlesrhonejr@gmail.com>
2018-03-30 15:49:03 -0700

    Fix some of business logic

--- js/scripts.js
+++ js/scripts.js
@@ -9,8 +9,11 @@ $(document).ready(function() {
     var userNum = parseInt($("#robotInput").val());
     var finalResult = robotize(userNum)
 
-    $(".results").show();
+    alert(robotize(userNum));
     $(".results").text(finalResult);
+    $(".results").show();
+
+
 
   });
 });
@@ -19,32 +22,20 @@ $(document).ready(function() {
 
 var robotize = function(number) {
 
-if (number%3 !== 0) { //If the number is NOT divisible by 3 ...
+if (number%3 !== 0 || number === 0) { //If the number is NOT divisible by 3 ...
 
-  var numberString =  number.toString(); //convert number to string
-  var numberSplit = numberString.split('');
-  var numberArray =[];
+  // var numberString =  number.toString(); //convert number to string
+  // var numberSplit = numberString.split('');
 
-  numberArray.push(numberSplit);
-
-  console.log(numberArray);
-  console.log("value after string and split =" + numberArray);
-  console.log(typeof numberString)
-
-  if (numberArray.includes('0')) { // '0'
+  if (number == '0') { // '0'
     return "Beep!";
-  }
+  };
 
-  if (numberArray.includes('1')){ //'1'
+  if (number == '1') { //'1'
     return "Boop!";
-  }
-}
-  else {
-    console.log("3 logic number = " + number);
-    if (number % 3 == 0) { //Checks condition of 3 or divisible by 3
-      return "I'm sorry Dave, I'm afraid I can't do that.";
-    } else {
-      return "BZZT. Does not compute."; //Returns error if no conditions met
+  };
+} else {
+  console.log("3 logic number = " + number); //Checks condition of 3 or divisible by 3
+    return "I'm sorry Dave, I'm afraid I can't do that.";
     };
   };
-};
