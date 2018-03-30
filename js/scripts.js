be34c157dd29091a8d2b7ef4d939d4c398b65a77
CJ Rhone <charlesrhonejr@gmail.com>
2018-03-30 13:45:39 -0700

    Add 'includes' method to test for 1's and 0's

--- js/scripts.js
+++ js/scripts.js
@@ -19,27 +19,32 @@ $(document).ready(function() {
 
 var robotize = function(number) {
 
-var numberString =  number.toString().split('');
+if (number%3 !== 0) { //If the number is NOT divisible by 3 ...
 
+  var numberString =  number.toString(); //convert number to string
+  var numberSplit = numberString.split('');
+  var numberArray =[];
 
+  numberArray.push(numberSplit);
 
+  console.log(numberArray);
+  console.log("value after string and split =" + numberArray);
+  console.log(typeof numberString)
 
-  console.log(numberString);
+  if (numberArray.includes('0')) { // '0'
+    return "Beep!";
+  }
 
-
-    if (numberString === '0') { // '0'
-      return "Beep!";
-    }
-
-    if (numberString === '1'){ //'1'
-      return "Boop!";
-    }
-
-    console.log(number);
-
+  if (numberArray.includes('1')){ //'1'
+    return "Boop!";
+  }
+}
+  else {
+    console.log("3 logic number = " + number);
     if (number % 3 == 0) { //Checks condition of 3 or divisible by 3
       return "I'm sorry Dave, I'm afraid I can't do that.";
     } else {
       return "BZZT. Does not compute."; //Returns error if no conditions met
     };
   };
+};
